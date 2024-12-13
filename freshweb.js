8c1ab7bd46ebf821bbbb0b8fb44019cdd2aae891
Owen-Fresh <owencook359@gmail.com>
2024-12-13 12:18:51 -0800

    Add files via upload

---
+++ freshweb.js
@@ -0,0 +1,154 @@
+const Welcome = document.querySelector('.Welcome')
+const openWelcome = document.querySelector('[data-welcome-target]')
+const closeWelcome = document.querySelector('[data-close-button]')
+
+openWelcome.addEventListener('click', () => {
+  Welcome.show();
+})
+
+closeWelcome.addEventListener('click', () => {
+  Welcome.close();
+})
+
+
+const About = document.querySelector('.About')
+const openAbout = document.querySelector('[data-About-target]')
+const closeAbout = document.querySelector('[data-About-close]')
+
+openAbout.addEventListener('click', () => {
+  About.show();
+})
+
+closeAbout.addEventListener('click', () => {
+  About.close();
+})
+
+
+const Work = document.querySelector('[data-Work]')
+const openWork = document.querySelector('[data-Work-target]')
+const closeWork = document.querySelector('[data-Work-close]')
+
+openWork.addEventListener('click', () => {
+  Work.show();
+})
+
+closeWork.addEventListener('click', () => {
+  Work.close();
+})
+
+
+const Working = document.querySelector('.Working-On')
+const openWorking = document.querySelector('[data-Working-target]')
+const closeWorking = document.querySelector('[data-Working-close]')
+
+openWorking.addEventListener('click', () => {
+  Working.show();
+})
+
+closeWorking.addEventListener('click', () => {
+  Working.close();
+})
+
+
+const Writing = document.querySelector('[data-MyWriting]')
+const openWriting = document.querySelector('[data-Writing-target]')
+const closeWriting = document.querySelector('[data-Writing-Close]')
+
+openWriting.addEventListener('click', () => {
+  Writing.show();
+})
+
+closeWriting.addEventListener('click', () => {
+  Writing.close();
+})
+
+
+const Coin = document.querySelector('[data-Coin]')
+const openCoin = document.querySelector('[data-Coin-target]')
+const closeCoin = document.querySelector('[data-Coin-close]')
+
+openCoin.addEventListener('click', () => {
+  Coin.show();
+})
+
+closeCoin.addEventListener('click', () => {
+  Coin.close();
+})
+
+
+const Air = document.querySelector('[data-Air]')
+const openAir = document.querySelector('[data-Air-target]')
+const closeAir = document.querySelector('[data-Air-close]')
+
+openAir.addEventListener('click', () => {
+  Air.show();
+})
+
+closeAir.addEventListener('click', () => {
+  Air.close();
+})
+
+
+const Beverage = document.querySelector('[data-Beverage]')
+const openBeverage = document.querySelector('[data-Beverage-target]')
+const closeBeverage = document.querySelector('[data-Beverage-close]')
+
+openBeverage.addEventListener('click', () => {
+  Beverage.show();
+})
+
+closeBeverage.addEventListener('click', () => {
+  Beverage.close();
+})
+
+
+const Gothic = document.querySelector('[data-Gothic]')
+const openGothic = document.querySelector('[data-Gothic-target]')
+const closeGothic = document.querySelector('[data-Gothic-close]')
+
+openGothic.addEventListener('click', () => {
+  Gothic.show();
+})
+
+closeGothic.addEventListener('click', () => {
+  Gothic.close();
+})
+
+
+const Sunrise = document.querySelector('[data-Sunrise]')
+const openSunrise = document.querySelector('[data-Sunrise-target]')
+const closeSunrise = document.querySelector('[data-Sunrise-close]')
+
+openSunrise.addEventListener('click', () => {
+  Sunrise.show();
+})
+
+closeSunrise.addEventListener('click', () => {
+  Sunrise.close();
+})
+
+
+const Leverless = document.querySelector('[data-Leverless]')
+const openLeverless = document.querySelector('[data-Lever-target]')
+const closeLeverless = document.querySelector('[data-Leverless-close]')
+
+openLeverless.addEventListener('click', () => {
+  Leverless.show();
+})
+
+closeLeverless.addEventListener('click', () => {
+  Leverless.close();
+})
+
+
+const Monopolies = document.querySelector('[data-Monopolies]')
+const openMonopolies = document.querySelector('[data-Monopolies-target]')
+const closeMonopolies = document.querySelector('[data-Monopolies-close]')
+
+openMonopolies.addEventListener('click', () => {
+  Monopolies.show();
+})
+
+closeMonopolies.addEventListener('click', () => {
+  Monopolies.close();
+})
